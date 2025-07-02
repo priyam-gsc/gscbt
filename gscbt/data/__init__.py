47cbb7929b735b8a6d5c3d943c485ef0ff93fcbc
priyam <priyam.trambadia@ibisp.in>
2025-07-02 11:11:21 +0000

    chore(data): clean up __init__.py by removing deprecated exports and exposing new utilities

--- gscbt/data/__init__.py
+++ gscbt/data/__init__.py
@@ -2,12 +2,6 @@ from .continuous import get_continuous, get
 from .outright import get_outright
 from .spread import get_spread
 
-from .spread_instrument import (
-    RollMethod,
-    DataType,
-    SpreadInstrument,
-)
-
 from .synthetic_builder_wrappers import(
     sbw_get_contractwise,
     sbw_get_spreadwise,
